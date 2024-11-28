932f67d42acf7612bb91c5f264fe84a36474b42c
SamyBravy <samuele@SurfaceSamuele.>
2024-11-28 14:21:19 +0100

    mancano solo i test di ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,33 +6,37 @@
 /*   By: samuele <samuele@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 22:20:20 by samuele           #+#    #+#             */
-/*   Updated: 2024/11/28 13:50:37 by samuele          ###   ########.fr       */
+/*   Updated: 2024/11/28 14:05:08 by samuele          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Cat.hpp"
-#include "Dog.hpp"
+#include "MateriaSource.hpp"
+#include "Character.hpp"
+#include "Ice.hpp"
+#include "Cure.hpp"
 
 int main()
 {
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
+    MateriaSource* src = new MateriaSource();
 
-    static_cast<const Dog *>(j)->getBrain()->setIdea(0, "I'm a dog");
-    static_cast<const Cat *>(i)->getBrain()->setIdea(0, "I'm a cat");
-    
-    std::cout << "Animal j: " << std::endl;
-    std::cout << j->getType() << " " << std::endl;
-    j->makeSound();
-    std::cout << "Idea: " << static_cast<const Dog *>(j)->getBrain()->getIdea(0) << std::endl;
-    
-    std::cout << "Animal i: " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound();
-    std::cout << "Idea: " << static_cast<const Cat *>(i)->getBrain()->getIdea(0) << std::endl;
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
 
-    delete j;
-    delete i;
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+    
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+
+    delete bob;
+    delete me;
+    delete src;
 
     return 0;
 }
